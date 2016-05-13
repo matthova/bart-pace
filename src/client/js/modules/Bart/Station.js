f8735f2c2748836fd2b900a4564376c59b21e253
bangarang <me@alexjrock.com>
2016-05-13 12:05:28 -0500

    Staion.js stuffs

--- src/client/js/modules/Bart/Station.js
+++ src/client/js/modules/Bart/Station.js
@@ -4,36 +4,14 @@ import React from 'react';
 export default class Station extends React.Component {
   constructor(props) {
     super(props);
-    this.state.minutes = props.info.minutes;
+    // this.state.minutes = props.info.minutes;
+    this.state = { minutes: props.info.minutes };
   }
-  componentWillMount() {
-    // console.log('componentWillMount');
-    return true;
-  }
-  componentDidMount() {
-    // console.log('componentDidMount');
-    return true;
-  }
-  componentWillReceiveProps(nextProps) {
-    // console.log('componentWillReceiveProps', nextProps);
-    return true;
-  }
-  shouldComponentUpdate() {
-    // console.log('shouldComponentUpdate');
-    return true;
-  }
-  componentWillUpdate() {
-    // console.log('componentWillUpdate');
-    return false;
-  }
-  componentDidUpdate() {
-    // console.log('componentDidUpdate');
-    return false;
-  }
+
   render() {
     console.log('render', this.state);
     return (
-      <div>{this.state.minutes}</div>
+      <div>{this.props.info.minutes}</div>
     );
   }
 }
